6f10dd73fd3aa9dab03d48416138d5a61d450300
tsukada0316 <zm26454@josai.ac.jp>
2026-06-22 12:02:00 +0900

    Add alert statements for math operations
    Added various alert statements for mathematical operations and tests.

--- myjs.js
+++ myjs.js
@@ -1,3 +1,11 @@
 alert('Hello, Anri!');
 alert(1+1);
 alert(Math.sqrt(1+1));
+2の平方根と3の平方根の和を計算しなさい。
+1を0で割るとどうなるのか、試してみなさい。
+0を0で割るとどうなるのか、試してみなさい。
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
